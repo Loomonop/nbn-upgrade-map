803b93948cebe9bbaf31ba8f0c8e20e6cdfaad11
Simon Roberts <lyricnz@gmail.com>
2023-05-30 09:24:30 +1000

    Sort suburbs by state+name before writing results/results.json

--- code/results.py
+++ code/results.py
@@ -1,26 +1,28 @@
 import datetime
+import glob
 import json
 import os
 
 if __name__ == "__main__":
-    suburb_record = {"suburbs": []}
-
     # check the folders in results
     states = ["ACT", "NSW", "NT", "QLD", "SA", "TAS", "VIC", "WA"]
 
+    suburbs = []
     for state in states:
-        # check if the folder exists
-        if not os.path.exists("results/" + state):
-            continue
-        for file in os.listdir("results/" + state):
-            if file.endswith(".geojson"):
-                suburb_record["suburbs"].append({
-                    "internal": file.split(".")[0].replace("-", " ").upper(),
-                    "state": state,
-                    "name": file.split(".")[0].replace("-", " ").title(),
-                    "file": file.split(".")[0],
-                    "date": datetime.datetime.fromtimestamp(os.path.getmtime("results/" + state + "/" + file)).strftime("%d-%m-%Y")
-                })
+        for file in glob.glob(f"results/{state}/*.geojson"):
+            filename, _ = os.path.splitext(os.path.basename(file))
+            suburbs.append({
+                "internal": filename.replace("-", " ").upper(),
+                "state": state,
+                "name": filename.replace("-", " ").title(),
+                "file": filename,
+                "date": datetime.datetime.fromtimestamp(os.path.getmtime(file)).strftime("%d-%m-%Y"),
+            })
+
+    # sort by state + name
+    suburb_record = {
+        "suburbs": sorted(suburbs, key=lambda k: (k["state"], k["name"]))
+    }
 
     with open("results/results.json", "w") as outfile:
         json.dump(suburb_record, outfile, indent=4)
