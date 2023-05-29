29ac774e833f0458d5330f05f7ab1465df242392
Simon Roberts <lyricnz@gmail.com>
2023-05-29 11:36:39 +1000

    Split main.py into NBN + DB

---
+++ code/nbn.py
@@ -0,0 +1,44 @@
+import logging
+import urllib.parse
+
+import diskcache
+import requests
+
+
+class NBNApi:
+    """Interacts with NBN's unofficial API."""
+    LOOKUP_URL = "https://places.nbnco.net.au/places/v1/autocomplete?query="
+    DETAIL_URL = "https://places.nbnco.net.au/places/v2/details/"
+    HEADERS = {
+        "referer": "https://www.nbnco.com.au/"
+    }
+
+    def __init__(self):
+        # 1GB LRU cache of gnaf_pid->loc_id and loc_id->details
+        self.cache = diskcache.Cache('cache', statistics=True)
+
+    def close(self):
+        # TODO Each thread that accesses a cache should also call close on the cache.
+        self.cache.close()
+        hits, misses = self.cache.stats(reset=True)
+        logging.info('Cache stats: %d hits, %d misses', hits, misses)
+
+    def get_nbn_data_json(self, url):
+        """Gets a JSON response from a URL."""
+        return requests.get(url, stream=True, headers=self.HEADERS).json()
+
+    def get_nbn_loc_id(self, key: str, address: str) -> str:
+        """Return the NBN locID for the provided address, or None if there was an error."""
+        if key in self.cache:
+            return self.cache[key]
+        loc_id = self.get_nbn_data_json(self.LOOKUP_URL + urllib.parse.quote(address))["suggestions"][0]["id"]
+        self.cache[key] = loc_id  # cache indefinitely
+        return loc_id
+
+    def get_nbn_loc_details(self, id: str) -> dict:
+        """Return the NBN details for the provided id, or None if there was an error."""
+        if id in self.cache:
+            return self.cache[id]
+        details = self.get_nbn_data_json(self.DETAIL_URL + id)
+        self.cache.set(id, details, expire=60 * 60 * 24 * 7)  # cache for 7 days
+        return details
